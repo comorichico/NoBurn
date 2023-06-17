c3ea248eed55a0cdeff4670ea4dd299452563677
comorichico <chico@comorichico.com>
2023-06-18 00:10:24 +0900

    ネザーゲートに点火できない不具合修正 火がついたら自動消火するように修正

--- src/main/java/com/comorichico/noburn/NoBurn.java
+++ src/main/java/com/comorichico/noburn/NoBurn.java
@@ -1,9 +1,11 @@
 package com.comorichico.noburn;
 
 import org.bukkit.Bukkit;
+import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBurnEvent;
+import org.bukkit.event.block.BlockIgniteEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class NoBurn extends JavaPlugin implements Listener{
@@ -14,7 +16,20 @@ public final class NoBurn extends JavaPlugin implements Listener{
     }
 
     @EventHandler
+    public void onBlockIgnite(BlockIgniteEvent event) {
+        if(null != event.getIgnitingBlock()){
+            if(event.getIgnitingBlock().getType() == Material.FIRE){
+                event.setCancelled(true);
+                event.getIgnitingBlock().setType(Material.AIR);
+            }
+        }
+    }
+
+    @EventHandler
     public void onBlockBurn(BlockBurnEvent event) {
-        event.setCancelled(true);
+        if(null != event.getIgnitingBlock()){
+            event.setCancelled(true);
+            event.getIgnitingBlock().setType(Material.AIR);
+        }
     }
 }
